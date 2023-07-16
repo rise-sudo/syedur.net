b516f96dbcad6b8a95c82726e363d9ca8eda1cd2
rise-sudo <syedur.app@gmail.com>
2023-07-16 12:09:20 -0400

    feat: basic css styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to Syedur.net</h1>
-<p>Here are the blogs: <a href='/blog'>blogs</a></p>
+<div class="bg-slate-800 min-h-screen flex">
+    <div class="grow"></div>
+    <div class="flex-none w-1/2">
+        <h1 class="text-3xl font-bold text-slate-400 mt-4 pb-2">Welcome to Syedur.net</h1>
+        <p class="text-slate-300 pb-2">Here are the blogs: <a class="hover:text-slate-100" href='/blog'>blogs</a></p>
+    </div>
+    <div class="grow"></div>
+</div>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,9 +2,16 @@
     import blogs from './blogs.json';
 </script>
 
-<h1>Blogs List</h1>
-<ul>
-    {#each blogs as blog}
-        <li><a href="blog/{blog.name}">{blog.name}</a></li>
-    {/each}
-</ul>
+<div class="bg-slate-800 min-h-screen flex">
+    <div class="grow"></div>
+    <div class="flex-none w-1/2">
+        <h1 class="text-3xl font-bold text-slate-400 mt-4">Blogs</h1>
+        <p class="text-slate-300 pb-2">Here are the available blogs:</p>
+        <ul>
+            {#each blogs as blog}
+                <li class="text-slate-600 hover:text-slate-400"><a href="blog/{blog.name}">{blog.name}</a></li>
+            {/each}
+        </ul>
+    </div>
+    <div class="grow"></div>
+</div>
