d236ca2a9eac1d3b154ef8097fa8d3be9200c934
Syedur Rahman <80500405+rise-sudo@users.noreply.github.com>
2023-07-16 12:10:46 -0400

    feat: basic css styling (#12)

--- src/routes/blog/[blog]/+page.svelte
+++ src/routes/blog/[blog]/+page.svelte
@@ -22,15 +22,23 @@
     
 </script>
 
-{#if data.content}
-    {#each data.html as blogLine}
-        {#if blogLine.html_tag == 'h1'}
-            <h1>{blogLine.content}</h1>
-        {:else if blogLine.html_tag == 'p'}
-            <p>{blogLine.content}</p>
+
+
+<div class="bg-slate-800 min-h-screen flex">
+    <div class="grow"></div>
+    <div class="flex-none w-1/2">
+        {#if data.content}
+            {#each data.html as blogLine}
+                {#if blogLine.html_tag == 'h1'}
+                    <h1 class="text-3xl font-bold text-slate-400 mt-4 pb-2">{blogLine.content}</h1>
+                {:else if blogLine.html_tag == 'p'}
+                    <p class="text-slate-300 pb-2">{blogLine.content}</p>
+                {/if}
+            {/each}
+        {:else}
+            <p>Invalid URL for blog post.</p>
         {/if}
-    {/each}
-{:else}
-    <p>Invalid URL for blog post.</p>
-{/if}
-<a href='/blog'>Go Back</a>
+        <a class="text-slate-600 hover:text-slate-400" href='/blog'>Go Back</a>
+    </div>
+    <div class="grow"></div>
+</div>
